9c980eee3451ac9a704084548cbd4bd215be67a7
enigmasck <timothyasanders@gmail.com>
2013-12-10 11:30:32 -0500

    figuring out the uri path

--- redirect_page.php
+++ redirect_page.php
@@ -1,18 +1,19 @@
 <?php
 session_start();
 
-//ini_set('display_errors', 'On');
-//error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+error_reporting(E_ALL | E_STRICT);
 
-echo count($POST);
-if(count($POST['yourName']) > 0){
+echo '<pre>'.var_dump($_POST).'</pre>';
+
+if(count($_POST['yourName']) > 0){
     $myForm['yourName'] = $_POST['yourName'];
     $myForm['yourAge'] = $_POST['yourAge'];
     
     $_SESSION['savedForm'] = $myForm;
-    
+    echo '<pre>'.var_dump($_SERVER['SCRIPT_FILENAME']).'</pre>';
     header("HTTP/1.1 303 See Other");
-    header("Location: http://$_SERVER[HTTP_HOST]/action.php");
+    //header("Location: http://$_SERVER[HTTP_HOST]/action.php");
     die();
 } else if(isset($_SESSION['savedForm'])) {
    
